e3287d3dfb796daf99d3223ee12d09138f319d14
tjayrush <jrush@quickblocks.io>
2022-04-01 07:24:12 -0400

    Updates bloom filters in golang code

--- src/apps/chifra/pkg/bloom/bloom.go
+++ src/apps/chifra/pkg/bloom/bloom.go
@@ -28,50 +28,91 @@ type BloomFilter struct {
 const (
 	BLOOM_WIDTH_IN_BITS  = (1048576)
 	BLOOM_WIDTH_IN_BYTES = (BLOOM_WIDTH_IN_BITS / 8)
+	MAX_ADDRS_IN_BLOOM   = 50000
 )
 
 func NewBloomFilter() BloomFilter {
 	var ret BloomFilter
-	ret.Blooms = make([]bloomBytes, 1)
+	ret.Blooms = make([]bloomBytes, 0, 1)
 	ret.Count = 1
 	return ret
 }
 
-func NewBloomFilterFromAddress(addr common.Address) BloomFilter {
-	ret := NewBloomFilter()
-	ret.addAddrToBloom(addr)
-	return ret
-}
-
-func chunkBytes(addr common.Address, chunkSize int) [][]byte {
-	var chunks [][]byte
+func bitsToLight(addr common.Address) (parts [5][]byte, vals [5]uint32, bits [5]uint32) {
+	cnt := 0
 	slice := addr.Bytes()
-	for i := 0; i < len(slice); i += chunkSize {
-		end := i + chunkSize
+	for i := 0; i < len(slice); i += 4 {
+		end := i + 4
 		if end > len(slice) {
 			end = len(slice)
 		}
-		chunks = append(chunks, slice[i:end])
+		bytes := slice[i:end]
+		parts[cnt] = bytes
+		vals[cnt] = binary.BigEndian.Uint32(bytes)
+		bits[cnt] = (vals[cnt] % uint32(BLOOM_WIDTH_IN_BITS))
+		cnt++
 	}
-	return chunks
+	return
 }
 
-// bloom_t addr_2_Bloom2(const address_t& addr, CUintArray& litBits) {
-//     bloom_t ret;
-//     cout << addr << " ";
-//     for (size_t k = 0; k < K; k++) {
-//         string_q four_byte = extract(addr, 2 + (k * NIBBLE_WID), NIBBLE_WID);
-//         uint64_t bit64 = str_2_Uint("0x" + four_byte);
-//         uint64_t bit = (bit64 % BLOOM_WIDTH_IN_BITS);
-//         ret.lightBit(bit);
-//         litBits.push_back(bit);
-//         cout << four_byte << "-" << uint_2_Str(bit64) << "-" << uint_2_Str(bit) << " ";
-//     }
-//     cout << endl;
-//     return ret;
-// }
-func (bloom *BloomFilter) addAddrToBloom(addr common.Address) error {
-	return nil
+//----------------------------------------------------------------------
+func (bloom *BloomFilter) getStats() (nBlooms uint64, nInserted uint64, nBitsLit uint64, nBitsNotLit uint64, sz uint64, bitsLit []uint64) {
+	bitsLit = []uint64{}
+	sz += 4
+	nBlooms = uint64(bloom.Count)
+	for _, bf := range bloom.Blooms {
+		nInserted += uint64(bf.NInserted)
+		sz += 4 + uint64(len(bf.Bytes))
+		for bitPos := 0; bitPos < len(bf.Bytes)*8; bitPos++ {
+			if isBitLit(uint32(bitPos), bf.Bytes) {
+				nBitsLit++
+				bitsLit = append(bitsLit, uint64(bitPos))
+			} else {
+				nBitsNotLit++
+				// fmt.Printf("%d", b)
+			}
+		}
+	}
+	return
+}
+
+//----------------------------------------------------------------------
+func (bb *bloomBytes) isInBloomBytes(addr common.Address) bool {
+	_, _, bitsLitInAddr := bitsToLight(addr)
+	// fmt.Println("Checking address", addr.Hex(), bitsLitInAddr)
+	for _, bit := range bitsLitInAddr {
+		// fmt.Println("\tTesting:", bit, isBitLit(bit, bb.Bytes))
+		if !isBitLit(bit, bb.Bytes) {
+			// fmt.Println("\t\tMISS")
+			return false
+			// } else {
+			// 	fmt.Println("\t\tHIT")
+		}
+	}
+	return true
+}
+
+//----------------------------------------------------------------------
+func (bloom *BloomFilter) isInBloomFilter(addr common.Address) bool {
+	// _, _, _, _, _, bitsLit := bloom.getStats()
+	// fmt.Println("\n------------------------------------\nbitsLit:", bitsLit)
+	for _, bb := range bloom.Blooms {
+		if bb.isInBloomBytes(addr) {
+			return true
+		}
+	}
+	return false
+}
+
+//---------------------------------------------------------------------------
+func isBitLit(bit uint32, bytes []byte) bool {
+	which := uint32(bit / 8)
+	index := uint32(BLOOM_WIDTH_IN_BYTES - which - 1)
+
+	whence := uint32(bit % 8)
+	mask := byte(1 << whence)
+
+	return (bytes[index]&mask != 0)
 }
 
 func (bloom *BloomFilter) ReadBloomFilter(fileName string) (err error) {
@@ -140,5 +181,26 @@ func (bloom *BloomFilter) DisplayBloom(verbose int) {
 			}
 		}
 		fmt.Println()
+	}
+}
+
+func (bloom *BloomFilter) LightBits(bits [5]uint32) {
+	if len(bloom.Blooms) == 0 {
+		bloom.Blooms = append(bloom.Blooms, bloomBytes{})
+		bloom.Blooms[0].Bytes = make([]byte, BLOOM_WIDTH_IN_BYTES)
+	}
+
+	loc := len(bloom.Blooms) - 1
+	for _, bit := range bits {
+		which := (bit / 8)
+		whence := (bit % 8)
+		index := BLOOM_WIDTH_IN_BYTES - which - 1
+		mask := uint8(1 << whence)
+		bloom.Blooms[loc].Bytes[index] |= mask
+	}
+	bloom.Blooms[loc].NInserted++
+
+	if bloom.Blooms[loc].NInserted > MAX_ADDRS_IN_BLOOM {
+		bloom.Blooms = append(bloom.Blooms, bloomBytes{})
 	}
 }
